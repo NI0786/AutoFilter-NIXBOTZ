87f3c2d9033c8d6f039a52ef82f772d45fad59ab
NᎥຮhanτ <nimovie08@gmail.com>
2025-02-06 07:42:41 +0530

    Update users_chats_db.py

--- database/users_chats_db.py
+++ database/users_chats_db.py
@@ -235,7 +235,7 @@ class Database:
         user_data = {"id": user_id, "expiry_time": expiry_time, "has_free_trial": True}
         await self.users.update_one({"id": user_id}, {"$set": user_data}, upsert=True)
         
-        async def set_thumbnail(self, id, file_id):
+    async def set_thumbnail(self, id, file_id):
         await self.col.update_one({'id': int(id)}, {'$set': {'file_id': file_id}})
 
     async def get_thumbnail(self, id):
