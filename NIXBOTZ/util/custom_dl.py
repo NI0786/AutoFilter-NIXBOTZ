d5c53918ece2ac37192994111b6a2b2c27001ab6
NᎥຮhanτ <nimovie08@gmail.com>
2025-02-27 23:36:13 +0530

    Update custom_dl.py

--- NIXBOTZ/util/custom_dl.py
+++ NIXBOTZ/util/custom_dl.py
@@ -3,12 +3,12 @@ import asyncio
 import logging
 from info import *
 from typing import Dict, Union
-from NX.bot import work_loads
+from NIXBOTZ.bot import work_loads
 from pyrogram import Client, utils, raw
-from NX.util.file_properties import get_file_ids
+from NIXBOTZ.util.file_properties import get_file_ids
 from pyrogram.session import Session, Auth
 from pyrogram.errors import AuthBytesInvalid
-from NX.server.exceptions import FIleNotFound
+from NIXBOTZ.server.exceptions import FIleNotFound
 from pyrogram.file_id import FileId, FileType, ThumbnailSource
 
 
